0380b8149b25b60578082993aeae504b7f478ee1
nabilakiranatha <nabilakiranatha@gmail.com>
2026-04-10 12:03:02 +0700

    first commit

--- hitungSKS1.java
+++ hitungSKS1.java
@@ -1,6 +1,5 @@
-//1. import class
 import java.util.Scanner;
-public class hitungSks { 
+public class hitungSKS1 { 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
